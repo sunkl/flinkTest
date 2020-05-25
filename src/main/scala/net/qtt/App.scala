c7a9d69a840ecf1b073dd8a489b57dcad9576867
sunkunlun@lechuan <sunkunlun@qutoutiao.net>
2020-05-25 21:35:27 +0800

    t2

--- src/main/scala/net/qtt/App.scala
+++ src/main/scala/net/qtt/App.scala
@@ -21,14 +21,11 @@ object App  {
     new FlinkKafkaConsumer[String](topic,new SimpleStringSchema(),props)
   }
   def main(args: Array[String]): Unit = {
-<<<<<<< HEAD
     //println("m1")
     //m2
     //m3
     //t2
-=======
     //t3
->>>>>>> 3e5caab... t2
     val streamEnv = StreamExecutionEnvironment.getExecutionEnvironment
     val stream: KeyedStream[(String, Int), Tuple] = streamEnv.addSource(kafkaConsumerInstance("test_topic"))
     .flatMap(line=>line.split(","))
